716a0a1e90603272e6903cf11d502111721be4af
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-04-18 15:25:58 -0500

    Add the Pictures

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import './App.css';
+import BeastData from "./data.json"
 import HornedBeast from './HornedBeast';
 // This function sets the attributes for the props object on the Main.js file 
 function Main() {
@@ -6,8 +7,14 @@ function Main() {
     <div>
     <main>This is the main</main>
     {/* This declares the atributes for the each HornedBeast tag*/}
-    <HornedBeast title = 'reindeer' description = 'This is a reindeer' image = 'https://facts.net/wp-content/uploads/2021/10/Portrait-of-a-reindeer-1536x1025.jpg'/>
-    <HornedBeast title = 'unicorn' description = 'This is a unicorn' image = 'https://cdn.lifefamilyfun.com/wp-content/uploads/How-To-Draw-a-Unicorn.jpg'/>
+
+    {BeastData.map(Beast => {
+        return (
+          <HornedBeast title={Beast.title} description={Beast.description} image={Beast.image_url}/>
+        );
+    })}
+    {/* <HornedBeast title = 'reindeer' description = 'This is a reindeer' image = 'https://facts.net/wp-content/uploads/2021/10/Portrait-of-a-reindeer-1536x1025.jpg'/>
+    <HornedBeast title = 'unicorn' description = 'This is a unicorn' image = 'https://cdn.lifefamilyfun.com/wp-content/uploads/How-To-Draw-a-Unicorn.jpg'/> */}
     </div>
   );
 }
