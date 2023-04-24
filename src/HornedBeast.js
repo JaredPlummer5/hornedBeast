9db00423635f04264c8c7fb53cb2bed637d4b71f
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-04-24 07:43:46 -0500

    Finished lab3

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import { Card, Button } from 'react-bootstrap'
-import selectedBeast from './selectedBeast'
+
 
 // The HornedBeast function returns a div that passes the title, description, and the image url from the Main.js file
 // Props is a prameter that gets passed to represent the HornedBeast tag
@@ -9,21 +9,34 @@ import selectedBeast from './selectedBeast'
 
 function HornedBeast(props) {
     const [clicks, setClicks] = useState(0);
-
     function state() {
         setClicks(clicks + 1);
         console.log(clicks);
     }
-   
+    //Defined a state variable to keep track of how many times the "Like" button has been clicked.
 
+    //Return a Card component that displays the title, description, image, and Like button. 
     return (
-        <Card onClick={selectedBeast} id={props.id} style={{ width: '18rem' }}>
+        <Card id={props.id} style={{ width: '18rem' }}>
 
             <h2>{props.title}</h2>
+            {/* This lines gets the title and description from that are being passed down as props from the Main.js file */}
             <p>{props.description}</p>
 
-            <img  src={props.image} class="mw-100" height={'200px'} alt={props.description} />
+            <img
+                //When the image is clicked, call the SelectedBeastFunction and updateFunction to set the clicked beast in the App state.
+                onClick={() => {
+                    props.updateFunction(true);
+                    props.SelectedBeastFunction(props.clickedBeastVariable);
+                }
+                }
+                // This lines gets the image and description from that are being passed down as props from the Main.js file 
+                src={props.image} class="mw-100" height={'200px'}
+                alt={props.description}
+            />
+            {/* This lines update the state of likes by calling the state function */}
             <Button onClick={state}>Like</Button>
+            {/* This line displays a heart and the state of clicks for each beast */}
             <p>&#9829;{clicks}</p>
 
         </Card>
@@ -33,3 +46,4 @@ function HornedBeast(props) {
 
 
 export default HornedBeast;
+
--- src/selectedBeast.js
+++ src/selectedBeast.js
@@ -1,10 +1,53 @@
-import './App.css';
-import { Modal } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import HornedBeast from "./HornedBeast";
+import { useState } from "react";
+//Import the Modal and Button components from the react-bootstrap library, and the useState hook from React.
+function SelectedBeast(props) {
+    const [show, setShow] = useState(props.clickedBeastVariable);
+    //Define the JSX for the modal, passing in the clickedBeastVariable and updateFunction as props from the Main.jd file.
 
-function selectedBeast(){
-    return(
-        <Modal>HI</Modal>
-    )
+
+    //Define the handleClose function, which sets the show state variable to false 
+    //and calls the updateFunction to close the modal in the App component.
+    const handleClose = () => {
+        setShow(false);
+        //Conditionally render the modal based on the value of the show state variable.
+        props.updateFunction(false);
+    };
+
+    return (
+        //Return the JSX for the modal, displaying the title, image, and description of the clicked beast. 
+        //When the Close button is clicked, call the handleClose function to close the modal.
+        <>
+            <Modal show={show} onHide={handleClose}>
+                {/* renders a modal component with a show property bound to 
+            the show state variable and an onHide property bound to the handleClose function. */}
+                <Modal.Header closeButton>
+                    {/* renders a header component for the modal with a close button. */}
+                    <Modal.Title class="d-flex justify-content-center">{show.title}</Modal.Title>
+                    {/* renders the title of the modal with the title property of the show state variable. */}
+
+                </Modal.Header>
+                <Modal.Body class="d-flex justify-content-center flex-wrap" >
+                    {/* renders the body of the modal. */}
+                    <img class="mw-100" height={'200px'} src={props.clickedBeastVariable.image_url} alt={props.clickedBeastVariable.title} />
+                    {/* renders an image element with the image_url property of the 
+                    props.clickedBeastVariable object passed as a prop to the component. */}
+                    <p>{props.clickedBeastVariable.description}</p>
+                    {/* renders a paragraph element with the description property of the 
+                    props.clickedBeastVariable object passed as a prop to the component. */}
+                </Modal.Body>
+                <Modal.Footer>
+                    {/* renders the footer of the modal. */}
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    {/* renders a button element with the variant property set to "secondary" and an onClick property bound to the `handleClose */}
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
 
-export default selectedBeast;
+export default SelectedBeast;
